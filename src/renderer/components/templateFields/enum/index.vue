ee00665de84927f141b66f24ee0b15a17c74aa0f
hqweay <hqweay@gmail.com>
2020-03-18 19:57:37 +0800

    feat(enum): display ENUM field of item info

--- src/renderer/components/templateFields/enum/index.vue
+++ src/renderer/components/templateFields/enum/index.vue
@@ -6,13 +6,26 @@
       :label="false"
       :disabled=" enumValue !== enumItem.state"
     >{{enumValue}}</el-radio>-->
-    <el-radio-group>
+    <el-radio-group v-if="!isEdit" v-model="enumItem.value.state">
       <el-radio-button
         v-for="enumValue in enumItem.value.enums"
         :key="enumValue"
         :label="enumValue"
         :disabled=" enumValue !== enumItem.value.state"
         v-on:click.prevent.self
+      ></el-radio-button>
+    </el-radio-group>
+
+    <el-radio-group
+      class="edit-enum"
+      v-if="isEdit"
+      v-model="enumItem.value.state"
+      :change="changeValue()"
+    >
+      <el-radio-button
+        v-for="enumValue in enumItem.value.enums"
+        :key="enumValue"
+        :label="enumValue"
       ></el-radio-button>
     </el-radio-group>
   </div>
@@ -27,7 +40,13 @@ import { Component, Vue, Prop, Watch } from "vue-property-decorator";
 export default class extends Vue {
   // @Prop() readonly item!: ItemType;
   @Prop() enumItem!: any;
+  @Prop() isEdit!: boolean;
+  newValue: string = "";
   created() {}
+  changeValue() {
+    // this.enumItem.value.state = this.newValue;
+    // console.log(this.newValue);
+  }
 }
 </script>
 <style lang='scss'>
